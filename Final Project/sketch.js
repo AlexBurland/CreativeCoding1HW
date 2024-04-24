78f47def7e96a813bce9feea1d5d7f22e2956be3
TwistedAura <wb220926@umconnect.umt.edu>
2024-04-23 20:35:32 -0600

    Added player sprites and movement

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,31 +1,112 @@
-var map;
-var mapx = 50;
-var mapy = -700;
+let map;
+let playerImage;
+let BACKGROUND;
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    e: {
+        pressed: false
+    }
+}
+
+class Sprite {
+    constructor(position, image) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+       image(this.image, this.position.x, this.position.y);
+    }
+
+}
 
 function preload() {
-    map = loadImage('MonsterBayMap.png', imageLoaded);
+  map = loadImage('MonsterBayMap.png');
+  playerImage = loadImage('PlayerDown.png');
 }
 
 function setup() {
-    createCanvas(1280, 720);
-    background(0);
+  createCanvas(1280, 720);
+  BACKGROUND = new Sprite(createVector(-2500, -2200), map);
 }
 
 function draw() {
-    fill(225, 225, 225);
-    rect(0, 0, 1280, 720);
-    imageMode(CENTER);
-    if (map) {
-        image(map, mapx, mapy);
-    }
+ background(255);
+
+// Draw the background
+   BACKGROUND.draw();
+
+ //player image crop
+ let cropX = 65;
+ let cropY = 0;
+ let cropWidth = 55;
+ let cropHeight = 68;
+
+// Draw the player
+  image(playerImage, width / 2 -playerImage.width / 2, height / 2 - playerImage.height / 2, cropWidth, cropHeight, cropX, cropY, cropWidth, cropHeight);
+
+  if (keys.w.pressed && lastKey === 'w') BACKGROUND.position.y += 3
+  else if (keys.a.pressed && lastKey === 'a') BACKGROUND.position.x += 3
+  else if (keys.s.pressed && lastKey === 's') BACKGROUND.position.y -= 3
+  else if (keys.d.pressed && lastKey === 'd') BACKGROUND.position.x -= 3
 }
 
-function imageLoaded() {
-    console.log("Image loaded successfully");
+
+//movement
+let lastKey = ''
+function keyPressed() {
+    switch (key) {
+      case 'w':
+       keys.w.pressed = true;
+       lastKey = 'w'
+        break;
+      case 'a':
+        keys.a.pressed = true;
+        lastKey = 'a'
+        break;
+      case 's':
+        keys.s.pressed = true;
+        lastKey = 's'
+        break;
+      case 'd':
+        keys.d.pressed = true;
+        lastKey = 'd'
+        break;
+      case 'e':
+        keys.e.pressed = true;
+        break;
+    }
+    console.log(keys);
 }
 
-function keyPressed(){
-    if(keyCode == UP_ARROW)
-    { mapy = mapy -5;
+function keyReleased() {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'e':
+            keys.e.pressed = false;
+            break;
     }
+    console.log(keys);
 }
