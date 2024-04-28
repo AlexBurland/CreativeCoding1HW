be11612d4a30e06904fc8e03ca78b4a3234fbe2f
TwistedAura <wb220926@umconnect.umt.edu>
2024-04-28 13:06:36 -0600

    Added Array for collision detection

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,6 +1,10 @@
 let map;
 let playerImage;
 let BACKGROUND;
+const offset = {
+  x:-2500,
+  y:-2200
+}
 const keys = {
     w: {
         pressed: false
@@ -19,6 +23,56 @@ const keys = {
     }
 }
 
+const collisionsMap = []
+for (let i =0; i < collisions.length; i += 80) {
+  collisionsMap.push(collisions.slice(i, 80 + i))
+}
+
+
+
+class Boundary {
+  static width =64;
+  static height =64;
+  constructor({position}) {
+    this.position = position;
+    this.width = 64;
+    this.height = 64;
+  }
+
+  draw() {
+    fill(255, 0, 0);
+    rect(this.position.x, this.position.y, this.width, this.height);
+   }
+}
+
+const boundaries = []
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 856)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y
+      }
+    })
+   )
+ })
+})
+
+console.log(boundaries);
+
+function preload() {
+  map = loadImage('MonsterBayMap.png');
+  playerImage = loadImage('PlayerDown.png');
+}
+
+
+
+
+
+
 class Sprite {
     constructor(position, image) {
         this.position = position;
@@ -31,22 +85,28 @@ class Sprite {
 
 }
 
-function preload() {
-  map = loadImage('MonsterBayMap.png');
-  playerImage = loadImage('PlayerDown.png');
-}
 
 function setup() {
   createCanvas(1280, 720);
-  BACKGROUND = new Sprite(createVector(-2500, -2200), map);
+  // Last working spot 2:35 in vid something up with this don't forget
+  //image in video = map for me
+  BACKGROUND = new Sprite(createVector(offset.x, offset.y), map);
 }
 
+const testBoundary = new Boundary({
+  position: {
+    x: -2400,
+    y: -2200
+  }
+})
 function draw() {
- background(255);
-
 // Draw the background
    BACKGROUND.draw();
 
+   boundaries.forEach(boundary => {
+   boundary.draw();
+   })
+    testBoundary.draw()
  //player image crop
  let cropX = 65;
  let cropY = 0;
@@ -56,12 +116,11 @@ function draw() {
 // Draw the player
   image(playerImage, width / 2 -playerImage.width / 2, height / 2 - playerImage.height / 2, cropWidth, cropHeight, cropX, cropY, cropWidth, cropHeight);
 
-  if (keys.w.pressed && lastKey === 'w') BACKGROUND.position.y += 3
-  else if (keys.a.pressed && lastKey === 'a') BACKGROUND.position.x += 3
-  else if (keys.s.pressed && lastKey === 's') BACKGROUND.position.y -= 3
-  else if (keys.d.pressed && lastKey === 'd') BACKGROUND.position.x -= 3
+  if (keys.w.pressed && lastKey === 'w') BACKGROUND.position.y += 5
+  else if (keys.a.pressed && lastKey === 'a') BACKGROUND.position.x += 5
+  else if (keys.s.pressed && lastKey === 's') BACKGROUND.position.y -= 5
+  else if (keys.d.pressed && lastKey === 'd') BACKGROUND.position.x -= 5
 }
-
 
 //movement
 let lastKey = ''
